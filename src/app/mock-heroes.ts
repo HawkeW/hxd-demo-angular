0d7504339e2eab3d5846462b6192e0a35da454a2
hughew <hughew@foxmail.com>
2021-01-14 18:37:26 +0800

    angular-demo

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: '好好博士' },
+  { id: 12, name: '纳寇' },
+  { id: 13, name: '爆裂骑士' },
+  { id: 14, name: '辛德瑞拉' },
+  { id: 15, name: '凯恩斯' },
+  { id: 16, name: '橡皮人' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: '智商博士' },
+  { id: 19, name: 'MAGA!' },
+  { id: 20, name: '鱼雷人' }
+];
